6b3105506f203e5cd972ad348cf412d9eaef139c
isx47590131 <isx47590131@i21.informatica.escoladeltreball.org>
2018-05-11 12:43:22 +0200

    Docker compose working

--- compose/radiusserver/get_ip.sh
+++ compose/radiusserver/get_ip.sh
@@ -5,7 +5,9 @@
 # el la configuración de los clietes
 #--------------------------------------------------------------------------------
 #ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ldapserver | cut -d . -f 1-3 )
-ip=$(hostname -i | cut -d . -f 1-3)
+#ip=$(hostname -i | cut -d . -f 1-3)
+#retallem el camp de ip i ens quedem amb els 3 primers digits
+ip=$(grep "ipv4" ../docker-compose.yml | head -n1 | cut -d : -f 2 | cut -d . -f 1-3)
 network="$ip.0"
 if [ -f 'clients.conf' ]
 then
--- compose/radiusserver/startup.sh
+++ compose/radiusserver/startup.sh
@@ -3,4 +3,9 @@
 #/usr/sbin/radiusd -X
 /bin/chown -R radiusd.radiusd /var/run/radiusd
 /usr/sbin/radiusd -C
-/usr/sbin/radiusd -d /etc/raddb
+/bin/bash -c " while ! nc -z 172.100.0.2 389;
+	       do 
+		 echo 'cant contact with ldap (ip 172.100.0.2)' &> /dev/stderr;
+		 sleep 2;
+	       done;" 
+/usr/sbin/radiusd -X #-d /etc/raddb
